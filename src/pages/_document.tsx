efacefcec9292e8585469ff4ad8a3d53da1dca21
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-08-27 00:40:40 +0530

    change

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -9,16 +9,16 @@ export default function Document() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta property="dscvr:canvas:version" content="vNext" />
 
-        <meta
+        {/* <meta
           http-equiv="Content-Security-Policy"
           content="default-src 'self'; 
              script-src 'self' 'unsafe-eval'; 
              style-src 'self' 'unsafe-inline'; 
              img-src cdn.dummyjson.com https://ipfs.dscvr.one https://media.dscvr.one https://media1.stg.dscvr.one data:; 
-             connect-src dummyjson.com https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com ; 
+             connect-src 'self' https://dummyjson.com/  https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com ; 
              font-src 'self';
              "
-        />
+        /> */}
       </Head>
       <body>
         <Main />
