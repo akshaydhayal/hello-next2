4c48f2c7027c53088f5d42628c38b7314cc39849
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-08-26 13:22:02 +0530

    init

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,20 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>DSCVR Example Canvas - Next.js</title>
+        <meta name="description" content="DSCVR Example Canvas - Next.js" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta property="dscvr:canvas:version" content="vNext" />
+
+        <meta http-equiv="Content-Security-Policy" content="default-src 'self'; 
+             script-src 'self' 'unsafe-eval'; 
+             style-src 'self' 'unsafe-inline'; 
+             img-src 'self' https://cdn.dummyjson.com/ https://my-canvas.com/* data:; 
+             connect-src 'self' https://dummyjson.com/ https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com ; 
+             font-src 'self';
+             "/>
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -3,11 +3,12 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
   name: string;
+  url:string
 };
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: "John Doe" });
+  res.status(200).json({ name: "John Doe", url: "https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/1.png" });
 }
