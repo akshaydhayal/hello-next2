8768d14e30c5f72d1ed877d98cc36ff9946acd41
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-08-27 04:39:41 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,28 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+// };
+
+// export default nextConfig;
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    async headers() {
+        return [
+          {
+            source: "/(.*)",
+            headers: [
+              {
+                key: "Content-Security-Policy",
+                value:
+                  "connect-src 'self' https://dummyjson.com/  https://api.dscvr.one https://api1.stg.dscvr.one https://*.helius-rpc.com; script-src 'self'; style-src 'self'; img-src https://cdn.dummyjson.com/*; ",
+              },
+            ],
+          },
+        ];
+    },
 };
 
 export default nextConfig;
